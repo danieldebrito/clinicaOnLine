9dd03721d9c662a0fdaed8d42e6d734b97d5eecb
unknown <danieldebrito@outlook.com>
2020-10-22 21:45:14 -0300

    turnera - login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAuU4vEaQvKdk4KFGiNnA6n0mL0CZL17BE',
+    authDomain: 'clinicaonline-745d6.firebaseapp.com',
+    databaseURL: 'https://clinicaonline-745d6.firebaseio.com',
+    projectId: 'clinicaonline-745d6',
+    storageBucket: 'clinicaonline-745d6.appspot.com',
+    messagingSenderId: '846739899006',
+    appId: '1:846739899006:web:e091f5a292c70b0b944e2f'
+  },
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
